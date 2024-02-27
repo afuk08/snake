1ee5183c16e3f448a8c362b5cc5404ca8c0308ea
afuk08 <timothyemmanuel08@gmail.com>
2024-02-27 19:50:09 +0700

    Initial commit

---
+++ snake.py
@@ -0,0 +1,32 @@
+import pygame
+import random
+
+# Initialize Pygame
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 600, 400
+GRID_SIZE = 20
+FPS = 10
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+# Directions
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+# Snake class
+class Snake:
+    def __init__(self):
+        self.length = 1
+        self.positions = [((WIDTH // 2), (HEIGHT // 2))]
+        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
+        self.color = WHITE
+
+    def get_head_position(self):
+        return self.positions[0]
